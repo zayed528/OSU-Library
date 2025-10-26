bc09a5fa82a66497f2c7a4faab6f9ef6c74d4560
Zayed Ali <109417588+zayed528@users.noreply.github.com>
2025-10-26 05:38:04 -0400

    Clean up unnecessary deployment files

--- api/index.py
+++
@@ -1,6 +0,0 @@
-"""Vercel serverless function handler for FastAPI"""
-from mangum import Mangum
-from app.main import app
-
-# Mangum wraps FastAPI for AWS Lambda/Vercel serverless
-handler = Mangum(app, lifespan="off")
